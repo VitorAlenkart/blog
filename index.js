c5f10c4fe8561d0d8359517883cc9c7643f717e2
VitorAlenkart <vitoralencargithub@gmail.com200~git config --global user.name VitorAlencarDev1>
2023-06-08 09:43:33 -0300

    Criando estrutura do projeto

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+
+// View Engine
+app.set('view engine','ejs');
+
+
+// Static
+app.use(express.static('public'));
+
+
+// Set body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+// Rotes
+app.get('/', (req,res) => {
+    res.send('Opa!')
+})
+
+
+// Server
+app.listen(8080, () => {
+    console.log('Servidor rodando');
+})
